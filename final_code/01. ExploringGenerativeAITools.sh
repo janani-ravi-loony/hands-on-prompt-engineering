6d106d3910941464ad60dde2fdaba71584837143
janani-ravi-loony <janani@loonycorn.com>
2025-04-17 17:09:15 +0530

    Add files via upload

---
+++ final_code/01. ExploringGenerativeAITools.sh
@@ -0,0 +1,225 @@
+--------------------------------------------------------------------------
+Prompting Using Generative AI
+--------------------------------------------------------------------------
+
+# Have a number of tools open
+
+https://chat.openai.com/ (using ChatGPT Plus)
+
+
+https://chat.openai.com/ (free account)
+
+https://gemini.google.com/ (paid account)
+
+# https://zapier.com/blog/gemini-vs-chatgpt/
+
+https://claude.ai/ (paid account)
+
+https://www.meta.ai/ (no login needed)
+
+https://copilot.microsoft.com/ (free account)
+
+https://www.perplexity.ai/ (free account)
+
+# Perplexity AI and ChatGPT are both AI tools, but they differ significantly in their primary function. Perplexity primarily functions as an AI search engine, focusing on real-time information retrieval and research, while ChatGPT is a conversational AI chatbot designed for natural language interaction and creative content generation. 
+
+
+-----------------
+
+# The free tier offers a lot more now
+
+https://help.openai.com/en/articles/9275245-using-chatgpt-s-free-tier-faq
+
+# If you're on a paid ChatGPT plan (Plus, Pro, Team, or Enterprise), you can switch between models with the model selector.
+
+# Users on the Free tier will be defaulted to GPT-4o with a limit on the number of messages they can send using GPT-4o, which will vary based on current usage and demand. When unavailable, Free tier users will be switched back to GPT-4o mini.
+# https://help.openai.com/en/articles/7864572-what-is-the-chatgpt-model-selector#h_894355bbe5
+
+
+# Can they browse the internet? Same prompt for all
+
+"""
+Can you browse popular newspapers from the US and tell me what today's headlines are?
+"""
+
+# - ChatGPT free
+# - ChatGPT plus
+# - Gemini
+# - Claude
+# - Meta AI
+# - Copilot
+# - Perplexity
+
+
+# All of them support web search
+
+
+-------------------------------------------------------------
+# Now switch to the paid account ChatGPT Plus
+
+# Show the model drop down
+
+# Go to this page to see explanations for the models
+https://help.openai.com/en/articles/7864572-what-is-the-chatgpt-model-selector
+
+
+# Show temporary chat
+
+# Click on the top-right and show the current plan
+
+# Click on "Customize ChatGPT" using the drop down on the top right
+
+# Click on "Settings" and show the following options
+
+Personalization
+Data Controls
+
+
+---------------------------------------------
+# ChatGPT Plus
+
+# Show some examples of prompts 
+
+# Very open-ended
+
+
+# Back to GPT 4 (Very open-ended prompt, will give you very open ended responses)
+What kind of software tools and technologies should I learn today?
+
+# Provide some context with your prompt, you will get more useful responses
+I like working with data analysis and interpretation. What tools and technologies should I learn today?
+
+---------------------------
+
+# New GPT-4o session - with no context
+
+# It has no idea and will ask a follow up question
+Could you recommend some places to stay there?
+
+# It will suggest places to stay in Vermont (remembers context)
+I have heard Vermont is beautiful at this time of the year
+
+# Follow up
+How should I get there from SFO?
+
+# Note that it remembers the previous context
+
+
+---------------------------
+# Compare models on politics (even neutral questions)
+
+# Ask the same question across the different models
+
+Can you give me your opinion on whether the tariffs imposed by Donald Trump on Apr 2, 2025 are good or bad?
+
+# All of them try to give balanced views
+
+----------------------------------------------------------------
+# Gemini
+
+# Uses a completely different model
+# Bard (now Gemini) used to be powered by Google’s LaMDA and PaLM 2 language models, but now  Gemini uses Gemini Flash 
+
+https://ai.google.dev/gemini-api/docs/models
+
+
+# Google’s Gemini language models – Pro, Ultra, and Nano – are “natively multimodal”, which means it’s trained a variety of inputs, not just text. Google has also fine-tuned the model with more multimodel information. They can also access the internet in real time.
+
+"""
+Can you tell me about why Nvidia had to take a 5.5B surcharge?
+"""
+
+# Click on 3 dots and double check responses (for the previous question on tariffs)
+
+# Notice that it highlights the sources
+
+# This is fresh news as of yesterday
+
+
+----------------------------------------------------------------
+# Anthropic's Claude
+
+# Claude is a family of large language models developed by Anthropic.[1] The first model was released in March 2023. Claude 3, released in March 2024, can also analyze images.[2]
+
+# Claude 3 Haiku: Fast and cost-effective
+
+# Claude 3 Sonnet: Balances speed and intelligence
+
+# Claude 3 Opus: Excels at complex tasks and higher-order math
+
+# Claude models are generative pre-trained transformers. They have been pre-trained to predict the next word in large amounts of text. Claude models have then been fine-tuned with Constitutional AI with the aim of making them helpful, honest, and harmless.[3][4]
+
+# Constitutional AI is an approach developed by Anthropic for training AI systems, particularly language models like Claude, to be harmless and helpful without relying on extensive human feedback. The method, detailed in the paper "Constitutional AI: Harmlessness from AI Feedback" involves two phases: supervised learning and reinforcement learning.[4]
+
+# In the supervised learning phase, the model generates responses to prompts, self-critiques these responses based on a set of guiding principles (a "constitution"), and revises the responses. Then the model is fine-tuned on these revised responses.[4]
+
+
+# Does not have latest information
+
+"""
+Can you tell me about why Nvidia had to take a 5.5B surcharge?
+
+"""
+
+# It does not know
+
+# Upload a PDF
+
+"The Wise Owl and the Impatient Rabbit.pdf"
+
+
+Who are the main characters in this story?
+
+What does Rosie learn at the end of the story?
+
+
+----------------------------------------------------------------
+# Microsoft Copilot
+
+# Uses multiple Open AI's models and other models to power its chat 
+
+# Copilot utilizes the Microsoft Prometheus model. This model is based on OpenAI's GPT-4, GPT-4 Turbo, and GPT-4o large language models. It also uses a component called the Orchestrator, which generates search queries to combine Bing search results with the OpenAI models. In addition, Copilot uses the DALL-E 3 model for image generation. 
+
+
+I need to quickly brush up on my Excel skills for a project. How do I start?
+
+
+Can you give me reference links?
+
+
+----------------------------------------------------------------
+# Meta AI
+
+# Completely free to use
+
+# Can generate images when logged in
+
+
+----------------------------------------------------------------
+# Leaderboard to compare models
+
+https://lmarena.ai/?leaderboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
